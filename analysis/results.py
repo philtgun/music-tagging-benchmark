723f2cbef18e25f29e0f7b18a6695d2798ee3aea
Philip Tovstogan <phil.tgun@gmail.com>
2020-04-15 18:40:06 +0200

    WIP: implementing common tags between 3 datasets

--- analysis/results.py
+++ analysis/results.py
@@ -1,9 +1,8 @@
 from pathlib import Path
 from argparse import ArgumentParser
-import itertools
 
 import pandas as pd
-import numpy as np
+from sklearn import metrics
 
 DATASETS = ['mtat', 'msd', 'jamendo']
 MODELS = ['musicnn', 'fcn', 'se']
@@ -15,26 +14,33 @@ def get_results(directory):
     directory = Path(directory)
     num_tracks = {dataset: pd.read_csv(directory / 'analysis' / f'{dataset}.csv', index_col=0) for dataset in DATASETS}
     tags = {dataset: item.index for dataset, item in num_tracks.items()}
+    common_tags = sorted(set.intersection(*[set(_tags) for _tags in tags.values()]))
+    print(common_tags)
 
     # ROC_AUC
     performance_results = {}
     for model in MODELS:
         performance_results[model] = {}
-        roc_aucs = pd.DataFrame(index=DATASETS, columns=DATASETS, dtype='float')
+        roc_aucs_all = pd.DataFrame(index=DATASETS, columns=DATASETS, dtype='float')
+        pr_aucs_all = pd.DataFrame(index=DATASETS, columns=DATASETS, dtype='float')
         for test_dataset in DATASETS:
             performance_results[model][test_dataset] = {}
             for train_dataset in DATASETS:
-                results_run = pd.read_csv(
-                    directory / 'results' / f'test-{test_dataset}-train-{train_dataset}-{model}' / 'results_report.csv',
-                    index_col=0)
-                performance_results[model][test_dataset][train_dataset] = results_run
-                if test_dataset == train_dataset:
-                    tags_common = set.union(*[set(num_tracks[dataset].index & num_tracks[test_dataset].index)
-                                              for dataset in DATASETS if not dataset == test_dataset])
-                else:
-                    tags_common = set(num_tracks[test_dataset].index & num_tracks[train_dataset].index)
-                roc_aucs.loc[train_dataset, test_dataset] = results_run.loc[tags_common, 'AUC'].mean()
-        roc_aucs.to_csv(directory / 'analysis' / f'{model}.csv', float_format='%.2f')
+                path = directory / 'results' / f'test-{test_dataset}-train-{train_dataset}-{model}'
+                predictions = pd.read_csv(path / 'est.csv', index_col=0, names=tags[train_dataset])
+                groundtruth = pd.read_csv(path / 'gt.csv', index_col=0, names=tags[test_dataset])
+
+                roc_aucs = metrics.roc_auc_score(groundtruth[common_tags], predictions[common_tags], average=None)
+                pr_aucs = metrics.average_precision_score(groundtruth[common_tags], predictions[common_tags], average=None)
+
+                print(roc_aucs)
+                print(pr_aucs)
+                input('break')
+
+                performance_results[model][test_dataset][train_dataset] = \
+                    pd.DataFrame()
+                roc_aucs_all.loc[train_dataset, test_dataset] = results_run.loc[tags_common, 'AUC'].mean()
+        roc_aucs_all.to_csv(directory / 'analysis' / f'{model}.csv', float_format='%.2f')
 
     # Compare different models on same pair of datasets
     model = 'musicnn'
